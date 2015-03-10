ce2f1aa51a3bd0e67f215359a673609f5395ce88
David Whale <david@thinkingbinaries.com>
2015-03-10 22:48:13 +0000

    Created design for the guessing game.

---
+++ guess_4lists.py
@@ -0,0 +1,40 @@
+# guess_4lists.py  10/03/2015  D.J.Whale
+#
+# A guessing game, using a binary tree structure
+# The binary tree is implemented using 4 lists
+# This is a version for children who don't know about lists of lists yet
+
+import time
+
+# load file from disk
+
+anothergo = True
+while anothergo:
+    # display banner
+
+    finished = False
+    pos = 0
+    
+    while not finished:
+        # is pos a branch node?
+        #   ask multi choice question
+        #   yes: take yes branch
+        #   no:  take no branch
+        
+        # else must be a leaf node
+        #   ask terminating question
+        #   did I get it right?
+        #       congratulate myself
+        #   not right
+        #       ask "what is it"
+        #       ask "distinguishing question"
+        #       ask "answer for your new item"
+        #   finished = True
+
+    # ask "another go"
+    # if no, anothergo = False
+
+# Thanks for playing the game
+# save file to disk
+
+    
